fd0bc660bf79c1e257b61ab83d891c3d05c6ec34
David Jelenc <david.jelenc@fri.uni-lj.si>
2018-10-04 13:31:27 +0200

    Adds dedicated message creates functions

--- file_encoder.py
+++ file_encoder.py
@@ -1,4 +1,6 @@
 import itertools
+import random
+import string
 
 from pyasn1.codec.ber import encoder, decoder
 from pyasn1.type import univ, char
@@ -6,7 +8,7 @@ from pyasn1.type import univ, char
 from trustmessages import DataRequest, Message, FormatRequest, DataResponse, Format, Rating, QtmDb, FormatResponse
 from trustmessages.trustutils import create_query, pp
 
-users = itertools.cycle(["a@x.com", "b@x.com", "c@x.com"])
+users = itertools.cycle(["aaaaaa@xxxxxx.com", "bbbbbb@xxxxxx.com", "cccccc@xxxxxx.com"])
 services = itertools.cycle(["buyer", "seller", "letter", "renter"])
 
 
@@ -19,15 +21,16 @@ def decode(filename, asn_type):
     print(pp(decoded))
 
 
-def encode(filename, pdu):
+def encode(filename, pdu, debug=True):
     with open(filename, "wb") as h:
         h.write(encoder.encode(pdu))
 
-    print(pp(pdu))
+    if debug:
+        print(pp(pdu))
     print("Saved to: '%s'" % filename)
 
 
-def enc_data_request():
+def enc_data_request(destination="../message-data-request.ber"):
     dr = DataRequest()
     dr["rid"] = 42
     dr["type"] = "assessment"
@@ -35,17 +38,17 @@ def enc_data_request():
     request = Message()
     request["version"] = 1
     request["payload"] = dr
-    encode("../message-data-request.ber", request)
+    encode(destination, request)
 
 
-def enc_format_request():
+def enc_format_request(destination="../message-format-request.ber"):
     request = Message()
     request["version"] = 1
     request["payload"] = FormatRequest(73)
-    encode("../message-format-request.ber", request)
+    encode(destination, request)
 
 
-def enc_data_response():
+def enc_data_response(destination="../message-data-response.ber"):
     a_res = DataResponse()
     a_res["provider"] = "ebay"
     a_res["format"] = Format((1, 1, 1))
@@ -68,10 +71,10 @@ def enc_data_response():
     response["version"] = 1
     response["payload"] = a_res
 
-    encode("../message-data-response.ber", response)
+    encode(destination, response)
 
 
-def enc_format_response():
+def enc_format_response(destination="../message-format-response.ber"):
     f_res = FormatResponse()
     f_res["rid"] = 100
     f_res["assessment-id"] = Format((1, 2, 3))
@@ -82,13 +85,78 @@ def enc_format_response():
     m = Message()
     m["version"] = 1
     m["payload"] = f_res
-    encode("../message-format-response.ber", m)
+    encode(destination, m)
+
+
+def enc_format_response_length(destination="../message-format-response-long.ber", length=100):
+    f_res = FormatResponse()
+    f_res["rid"] = 1
+    f_res["assessment-id"] = Format((1, 2, 3))
+    f_res["trust-id"] = Format((1, 2, 3))
+    f_res["assessment-def"] = char.PrintableString(generate_text(length))
+    f_res["trust-def"] = char.PrintableString(generate_text(length))
+
+    m = Message()
+    m["version"] = 1
+    m["payload"] = f_res
+    encode(destination, m, False)
+
+
+def generate_text(length=100):
+    return ''.join(random.choice(string.ascii_letters + " ") for x in range(length))
+
+
+def generate_constraints(length=10):
+    fields = ("source", "target", "service", "date")
+    comparisons = ("=", "<=", "<", ">", ">=")
+
+    generated = []
+    for _ in range(length):
+        field = random.choice(fields)
+
+        if field in ("source", "target"):
+            value = next(users)
+            comparison = "="
+        elif field == "service":
+            value = next(services)
+            comparison = "="
+        else:  # date
+            value = random.randint(0, 200000)
+            comparison = random.choice(comparisons)
+
+        generated.append("%s %s %s" % (field, comparison, value))
+
+    return generated
+
+
+def generate_query(length=10):
+    operators = ("AND", "OR")
+    query, *rest = generate_constraints(length)
+
+    for con in rest:
+        operator = random.choice(operators)
+        query += " %s %s" % (operator, con)
+
+    return query
+
+
+def enc_data_request_length(destination="../message-data-request.ber", length=10):
+    dr = DataRequest()
+    dr["rid"] = 1
+    dr["type"] = "assessment"
+    dr["query"] = create_query(generate_query(length))
+    request = Message()
+    request["version"] = 1
+    request["payload"] = dr
+    encode(destination, request, False)
 
 
 if __name__ == '__main__':
-    enc_format_request()
-    enc_format_response()
-    enc_data_request()
-    enc_data_response()
-
-    # decode("../message-format-response.ber", Message)
+    # enc_format_request()
+    # enc_format_response()
+    # enc_data_request()
+    # enc_data_response()
+    # decode("../c-dr.ber", Message)
+    # enc_format_response_length("../long-message-format-response.ber", 2**20)
+    enc_data_request_length("../long-message-data-request.ber", 16)
+    decode("../long-message-data-request.ber", Message)
